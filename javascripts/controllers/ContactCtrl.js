c0f5de1cd4839365c16a61725a27733e686e5bbc
Dani Adkins <danirenaeadkins12@gmail.com>
2016-11-19 08:15:00 -0600

    added functionality and styling

--- javascripts/controllers/ContactCtrl.js
+++ javascripts/controllers/ContactCtrl.js
@@ -1,6 +1,40 @@
 "use strict";
 
-app.controller("NavCtrl", function($scope, ContactFactory){
+app.controller("ContactCtrl", function($scope, ContactFactory){
   $scope.welcome = "hello"; //what exactly does this do?
-  // $scope.
+  $scope.showHide = true;
+  $scope.newContact = {};
+  $scope.contacts = [];
+
+
+let getTheContacts = function(){
+  ContactFactory.getContacts().then(function(fbContacts){
+    $scope.contacts = fbContacts;
+  });
+};
+
+getTheContacts();
+
+$scope.contactsListed = function() {
+  $scope.showHide = true;
+};
+
+
+  //   $scope.moreInfo = function() {
+  //   $scope.showHide = false;
+  // };
+
+$scope.addContact = function (){
+  // $scope.newContact.isCompleted = false; //change this
+  ContactFactory.addNewContact($scope.newContact).then(function(contactId){
+    getTheContacts();
+    $scope.newContact =  "";
+    // $scope.showListView = true; //change this
+    });
+  };
+
 });
+
+
+
+
--- javascripts/factories/ContactFactory.js
+++ javascripts/factories/ContactFactory.js
@@ -8,16 +8,40 @@ app.factory("ContactFactory", function($q, $http, FIREBASE_CONFIG){
           let contacts = [];
           Object.keys(response).forEach(function(key){
             response[key].id = key;
-            items.push(response[key]);
+            contacts.push(response[key]);
           });
           resolve(contacts);
         })
         .error(function(errorResponse){
           reject(errorResponse);
-        });
       });
-    };
+    });
+  }
 
-return {getContacts:getContacts}
+  var addNewContact = function(newContact){
+    return $q((resolve, reject)=>{
+      $http.post(`${FIREBASE_CONFIG.databaseURL}/contacts.json`,
+        JSON.stringify({
+          fullname: newContact.fullname,
+          phonenumber: newContact.phonenumber,
+          address: newContact.address,
+          city: newContact.city,
+          state: newContact.state,
+          zip: newContact.zip
+      })
+    )
+      .success(function(postResponse){
+        resolve(postResponse);
+      })
+      .error(function(postError){
+        reject(postError)
+      });
+    });
+  };
+
+return {getContacts:getContacts, addNewContact:addNewContact}
 
 });
+
+
+
